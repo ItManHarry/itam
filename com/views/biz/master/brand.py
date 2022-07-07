e4db9c8e9fc619b0ba5cf145dcaec79427e755db
itmanharry <jack_chengqian@163.com>
2022-07-07 13:17:26 +0800

    brand module

--- com/views/biz/master/brand.py
+++ com/views/biz/master/brand.py
@@ -1,6 +1,6 @@
-from flask import Blueprint, render_template, flash, redirect, url_for, request, session, current_app
+from flask import Blueprint, render_template, flash, redirect, url_for, request, session, current_app, jsonify
 from flask_login import login_required, current_user
-from com.models import BizBrandMaster
+from com.models import BizBrandMaster, BizBrandModel
 from com.plugins import db
 from com.decorators import log_record
 from com.forms.biz.master.brand import BrandSearchForm, BrandForm
@@ -64,3 +64,51 @@ def edit(id):
         flash('品牌信息更新成功！')
         return redirect(url_for('.index'))
     return render_template('biz/master/brand/edit.html', form=form)
+@bp_brand.route('/models/<brand_id>', methods=['POST'])
+@login_required
+@log_record('获取品牌型号信息')
+def models(brand_id):
+    brand = BizBrandMaster.query.get_or_404(brand_id)
+    models = [(model.id, model.name, model.code if model.code else '') for model in brand.models]
+    '''
+    for model in brand.models:
+        models.append((model.id, model.code, model.name if model.name else ''))
+    '''
+    return jsonify(models=sorted(models, key=lambda e: e[2]))
+@bp_brand.route('/model_add', methods=['POST'])
+@login_required
+@log_record('维护品牌型号信息')
+def model_add():
+    data = request.get_json()
+    brand_id = data['brand_id']
+    brand = BizBrandMaster.query.get_or_404(brand_id)
+    # 先移除已关联的型号值
+    for model in brand.models:
+        brand.models.remove(model)
+        db.session.commit()
+    # 移除的型号信息执行删除
+    removed = data['removed']
+    print('Removed : ', removed)
+    for model_id in removed:
+        model = BizBrandModel.query.get(model_id)
+        if model:
+            db.session.delete(model)
+            db.session.commit()
+    # 关联型号信息
+    models = data['p_models']
+    for model in models:
+        print('Model id : ', model['id'], ', code : ', model['code'], ', display : ', model['name'])
+        model_entity = BizBrandModel.query.get(str(model['id']))
+        if model_entity:
+            model_entity.code = model['code']
+            model_entity.name = model['name']
+            model_entity.update_id = current_user.id
+            model_entity.updatetime_utc = datetime.utcfromtimestamp(time.time())
+            model_entity.updatetime_loc = datetime.fromtimestamp(time.time())
+            db.session.commit()
+        else:
+            model_entity = BizBrandModel(id=uuid.uuid4().hex, code=model['code'], name=model['name'], create_id=current_user.id)
+            db.session.add(model_entity)
+        brand.models.append(model_entity)
+    db.session.commit()
+    return jsonify(code=1, message='型号信息维护成功!')
